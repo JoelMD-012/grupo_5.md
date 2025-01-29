32c71a873da4d97637387c718f92831f146b2502
Joel Miranda <joelmiranda006@gmail.com>
2025-01-28 20:54:39 -0400

    Primer commit: Segunda prueba BOA

---
+++ BoaPeroBienHecho/Program.cs
@@ -0,0 +1,33 @@
+using BoaPeroBienHecho.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://ctzyanhnhuwdvschboma.supabase.co/rest/v1/")
+});
+
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
